415724557e10b9a0004dbbb0ce46e7ca57b69ce1
Sonja <son.boschman@gmail.com>
2016-07-26 15:09:11 +0200

    single database connection sorted; index displays artikel table + img

--- src/main/java/controller/ControllerServlet.java
+++ src/main/java/controller/ControllerServlet.java
@@ -1,15 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 import java.io.IOException;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import session.ArtikelFacade;
 
 /**
  *
@@ -17,8 +14,17 @@ import javax.servlet.http.HttpServletResponse;
  */
 
 public class ControllerServlet extends HttpServlet {
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    
+    @EJB
+    private ArtikelFacade artikelFacade;
+    
+    @Override
+    public void init() throws ServletException {
+        
+        //bewaar de producten in de servlet
+        getServletContext().setAttribute("artikelen", artikelFacade.findAll());
+    }
+    
     /**
      * Handles the HTTP <code>GET</code> method.
      *
