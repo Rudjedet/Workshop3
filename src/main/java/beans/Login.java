161faf15af474f36f55cb1752b611ec2bddf2ddc
Sonja <son.boschman@gmail.com>
2016-07-29 13:00:16 +0200

    Inlogfunctie poging 1

---
+++ src/main/java/beans/Login.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import java.io.Serializable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import util.DataConnect;
+
+/**
+ *
+ * @author Sonja
+ */
+
+@ManagedBean
+@SessionScoped
+public class Login implements Serializable {
+    
+    private static final long serialVersionUID = 1094801825228386363L;
+    
+    private String pwd;
+    private String msg;
+    private String user;
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+    
+    public String validateUsernamePassword() {
+        boolean valid = Login.validate(user, pwd); 
+        if (valid) {
+            HttpSession session = SessionUtils.getSession();
+            session.setAttribute("username", user);
+            return "inlogGelukt";
+        }
+        else {
+            FacesContext.getCurrentInstance().addMessage
+                (null, new FacesMessage //(FacesMessage.SEVERITY_WARN, 
+                    ("De gebruikersnaam en/of het wachtwoord dat u heeft "
+                            + "ingevoerd is onbekend. Probeer opnieuw."));
+            return "login";
+        }
+    }
+    
+    public String logout() {
+        HttpSession session = SessionUtils.getSession();
+        session.invalidate();
+        
+        return "login";
+    }
+    
+    public static boolean validate(String user, String password) {
+        //Connection con = null;
+        //PreparedStatement pS = null;
+        
+        try (Connection con = (Connection) DataConnect.getConnection();) {
+            //con = (Connection) DataConnect.getConnection();
+            String query = "SELECT username, wachtwoord FROM account WHERE username = ? and wachtwoord = ?";
+            PreparedStatement pS = (PreparedStatement) con.prepareStatement(query);
+            
+            pS.setString(1, user);
+            pS.setString(2, password);
+            
+            ResultSet rS = pS.executeQuery();
+            
+            if (rS.next()) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            System.out.println("Login error:" + ex.getMessage());
+            
+            return false;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/util/DataConnect.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Sonja
+ */
+public class DataConnect {
+    
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection
+             ("jdbc:mysql://localhost:3306/mummiefabriekdb", "Sonja", "Asgard");
+            
+            return con;
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DataConnect.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        return null;
+    }
+    
+    public static void close(Connection con) {
+        try { 
+            con.close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(DataConnect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
